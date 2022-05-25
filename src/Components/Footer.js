a600a97cb2bdace873dee895f6f5b067151ff277
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-25 09:53:07 +0300

    add a footer section to the homepage

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div>
+         <footer className=" footer p-4 rounded-lg shadow md:flex md:items-center  md:p-6 dark:bg-gray-800 ">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">CocktailU-Space</a>. Make yourself a drink.
+            </span>
+        </footer> 
+        </div>
+    ) 
+}
+        
+   
+
+export default Footer;

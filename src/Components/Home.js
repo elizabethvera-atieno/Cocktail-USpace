79c1591bfc615646c81f0414635fad57377caa04
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-25 12:46:09 +0300

    fetch data and pass as props to cards

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,26 @@
 import React from "react";
 import About from "./About";
 import Search from "./Search";
-// import Cards from "./Cards";
+import Cards from "./Cards";
 import Feedback from "./Feedback";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 function Home(){
-    console.log("I am home");
+    const[drinks, setDrinks]=useState([])
+
+    useEffect(()=>{
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=m`)
+        .then((r)=> r.json())
+        .then((data)=>(setDrinks(data)))
+    },[])
+
     return(
         <div>
             <About/>
             <Search/>
-            {/* <Cards/> */}
+            {drinks.map((drink)=> <Cards key={drink} drinks={drink}/>)}
             <Feedback/>
             
           

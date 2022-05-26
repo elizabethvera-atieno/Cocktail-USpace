03b495c8381639833c359790ba02d733db90966d
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-26 13:18:19 +0300

    fetch data by id to diplay individual cocktail information

--- src/Components/Dispay.js
+++ src/Components/Dispay.js
@@ -1,36 +1,58 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function Display(){
+function Display({}){
+    const [drinkData, setDrinkData]=useState([])
+
+    useEffect(()=>{
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((r)=> r.json())
+        .then((data)=>(setDrinkData(data.drinks)))
+    },[])
+
+    const id = Number(window.location.pathname.split("/").pop())
+    console.log(id)
+    console.log(drinkData)
+
+
+
     return(
     <>
-    <div className=" pt-10 text-center"><h2 className="text-2xl text-white font-bold">Cocktail Name</h2></div>
-    <div className="pt-10 place-content-center flex ">
-        <div className=" max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <img
-            className="rounded-t-lg "
-            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-            alt=""
-            />
-
-            <div className="flex justify-between p-1">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-textcolor dark:text-white">
-                My CARD WAHALA
-                </h5>
+       {drinkData.map((drink)=>(
+            <div key={drink.idDrink}>
+            <div className=" pt-10 text-center"><h2 className="text-2xl text-textcolor font-bold">{drink.strDrink}</h2></div>
+            <div className="pt-10 place-content-center flex ">
+                <div className=" max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <img
+                className="rounded-t-lg "
+                src={drink.strDrinkThumb}
+                alt=""
+                />
+    
+                <div className="flex justify-between p-1">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-textcolor dark:text-white">
+                    {drink.strDrink}
+                    </h5>
+                </div>
             </div>
         </div>
-    </div>
-    <div className="text-center pt-5">
-       <h2 className="text-2xl text-white font-bold">Ingredients:</h2>
-        <ul className="text-white text-xl">
-            <li>one</li>
-            <li>two</li>
-            <li>two</li>
-        </ul>
-        <h2 className="text-2xl text-white font-bold">Method:</h2>
-        <p className="text-white text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lor
-        em Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-    </div>
-    
+        <div className="text-center pt-5">
+           <h2 className="text-2xl text-textcolor font-bold">Ingredients:</h2>
+            <ul className="text-white text-xl">
+                <li>{drink.strIngredient1}</li>
+                <li>{drink.strIngredient2}</li>
+                <li>{drink.strIngredient3}</li>
+            </ul>
+            <h2 className="text-2xl text-textcolor font-bold">Glass:</h2>
+            <p className="text-white text-xl">{drink.strGlass}</p>
+            <h2 className="text-2xl text-textcolor font-bold">Alcohol:</h2>
+            <p className="text-white text-xl">{drink.strAlcoholic}</p>
+            <h2 className="text-2xl text-textcolor font-bold">Method:</h2>
+            <p className="text-white text-xl">{drink.strInstructions}</p>
+        </div>
+            </div>
+       ))}
     </>
 
         

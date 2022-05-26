d4217c5ae8ecb0ea35b0c38bb2c22c586e9df58d
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-26 15:16:00 +0300

    add a heart shape icon to button

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -30,21 +30,10 @@ function Cards({Drinks}){
                                        </h5>
                                         <a
                                            href="#"
-                                           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-textcolor rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                        >
                                            Like
-                                           <svg
-                                               className="ml-2 -mr-1 w-4 h-4"
-                                               fill="currentColor"
-                                               viewBox="0 0 20 20"
-                                               xmlns="http://www.w3.org/2000/svg"
-                                           >
-                                               <path
-                                                   fillRule="evenodd"
-                                                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                                   clipRule="evenodd"
-                                               ></path>
-                                           </svg>
+                                           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                                        </a>
                                    </div>
                                </div>

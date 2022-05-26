10240ef5e22e360d596f0118319687932e0cfb61
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-27 00:30:16 +0300

    make site responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,25 @@
 import React from "react";
 
-function Navbar(){
+function Navbar() {
+
     return (
-        <nav className="navbar flex justify-between px-5">
-            <h2 className=" text-2xl pl-6 pt-3 text-textcolor">Cocktail U-Space</h2>
-            <h2 className=" text-2xl pl-6 pt-3 text-textcolor">
-                <a href="/">Home</a>
-            </h2>
-            <h2 className=" text-2xl pl-6 pt-3 text-textcolor">
-                <a href="/favorites">Favorites</a>
-            </h2>
-            <h2 className=" text-2xl pl-6 pt-3 text-textcolor">
-                <a href="/authentication">Login/Sign-up</a>
-            </h2>
+        <nav className="navbar md:flex justify-between px-5 border-gray-200 px-2 sm:px-4  rounded">
+            <h2 className=" md:text-xl pl-6 pt-3 text-textcolor">Cocktail U-Space</h2>
+            <div className="flex md:justify-end  w-full  " id="mobile-menu">
+                <h2 className=" md:text-xl pl-6 pt-3 text-textcolor">
+                    <a href="/">Home</a>
+                </h2>
+                <h2 className=" md:text-xl pl-4 pt-3 text-textcolor">
+                    <a href="/favorites">Favorites</a>
+                </h2>
+                <h2 className=" md:text-xl pl-4 pt-3 text-textcolor">
+                    <a href="/authentication">Login/Sign-up</a>
+                </h2>
+            </div>
         </nav>
+
+
+
     )
 }
 

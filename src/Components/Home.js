8a7bbfe07b7b97f32acb9cba0f60a17480121ba1
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-26 13:20:47 +0300

    remove display component as child of home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,19 +3,24 @@ import About from "./About";
 import Search from "./Search";
 import Cards from "./Cards";
 import Feedback from "./Feedback";
+// import Display from './Dispay';
 import { useEffect } from "react";
 import { useState } from "react";
+import { Route, useRouteMatch } from "react-router-dom";
 
 
 function Home(){
     const [drinkData, setDrinkData]=useState([])
+
+    const match = useRouteMatch();
+    console.log(match);
 
     useEffect(()=>{
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=m`)
         .then((r)=> r.json())
         .then((data)=>(setDrinkData(data.drinks)))
     },[])
-    console.log(drinkData)
+    // console.log(drinkData)
 
 
     return(
@@ -24,6 +29,9 @@ function Home(){
             <Search updateDrinks={setDrinkData} />
             <Cards Drinks={drinkData} />
             <Feedback/>
+           {/* <Route path={`/display`}>
+               <Display Drinks={drinkData}/>
+           </Route> */}
             
           
         </div>

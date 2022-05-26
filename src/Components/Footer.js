b7ca7d6f50d4e2aae0d334ad11cba65e452ff8d9
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-26 13:19:34 +0300

    alighn text to center

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <div>
-         <footer className=" footer p-4 rounded-lg shadow md:flex md:items-center  md:p-6 dark:bg-gray-800 ">
+         <footer className=" footer p-4 rounded-lg shadow md:flex md:items-center  md:p-6 dark:bg-gray-800 text-center">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" className="hover:underline">CocktailU-Space</a>. Make yourself a drink.
             </span>
         </footer> 

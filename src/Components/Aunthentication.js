908473c0c04268fcfe7ce963af85885ee0019dce
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-27 00:36:54 +0300

    use firebase for aunthetication forms

--- src/Components/Aunthentication.js
+++ src/Components/Aunthentication.js
@@ -1,55 +1,167 @@
 import React from "react";
+import { useState } from "react";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import {auth} from "./firebase-config";
 
 function Aunthentication() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth,(currentUser) =>{
+    setUser(currentUser)
+  })
+
+  function handleRegisterSubmit(event){
+    event.preventDefault()
+    async function register(){
+      try{
+        const user = await createUserWithEmailAndPassword(auth,loginEmail, loginPassword)
+        console.log(user)
+      } catch (error) {
+        console.log(error.message)
+      }
+  
+    };
+  }
+
+  function handleLoginSubmit(event){
+    event.preventDefault()
+    async function login(){
+      try{
+        const user = await signInWithEmailAndPassword(auth,registerEmail, registerPassword)
+        console.log(user)
+      } catch (error) {
+        console.log(error.message)
+      }
+  
+    };
+  }
+
+  const logout = async () => {
+
+    await signOut (auth);
+  };
+
   return (
     <>
       <div className="pt-10 place-content-center flex ">
         <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6">
-            <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign up to create an account</h5>
+          <form className="space-y-6" onSubmit={handleRegisterSubmit}>
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign up to create an account
+            </h5>
             <div>
-              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="name@company.com"
+                required
+                onChange={(event)=>{setRegisterEmail(event.target.value)}}
+              />
             </div>
             <div>
-              <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+                onChange={(event)=>{setRegisterPassword(event.target.value)}}
+              />
             </div>
             <div className="flex items-start">
               <div className="flex items-start">
-                <div className="flex items-center h-5">
-                </div>
+                <div className="flex items-center h-5"></div>
               </div>
             </div>
-            <button type="submit" className="w-full text-white bg-textcolor hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
+            <button
+              type="submit"
+              className="w-full text-white bg-textcolor hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sign up
+            </button>
           </form>
         </div>
 
         <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6">
-            <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+          <form className="space-y-6" onSubmit={handleLoginSubmit}>
+            <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign in to our platform
+            </h5>
             <div>
-              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder="name@company.com"
+                required
+                onChange={(event)=>{setLoginEmail(event.target.value)}}
+              />
             </div>
             <div>
-              <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                required
+                onChange={(event)=>{setLoginPassword(event.target.value)}}
+              />
             </div>
             <div className="flex items-start">
               <div className="flex items-start">
-                <div className="flex items-center h-5">
-                </div>
+                <div className="flex items-center h-5"></div>
               </div>
             </div>
-            <button type="submit" className="w-full text-white bg-textcolor hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+            <button
+              type="submit"
+              className="w-full text-white bg-textcolor hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Login to your account
+            </button>
           </form>
         </div>
+        <p className="text-white">Current user:</p>
+        {/* {user?.email} */}
+      </div>
+      <div className="pt-10 place-content-center flex ">
+      <button
+      className=" text-white bg-textcolor hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      onClick={logout}
+      >
+        Sign out
+      </button>
       </div>
     </>
-
-
-  )
+  );
 }
 
 export default Aunthentication;
